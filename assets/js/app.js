d26d959fae11f0fd2f674c573e7aedcf7d76d023
SofiaParodi <sofia.parodi503@gmail.com>
2024-09-09 13:47:13 -0300

    feat: añado elementos al array categories

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,27 @@
+ /* categories */
+ const categoriesArray = ['Comida', 'Servicios', 'Salidas', 'Educación', 'Transporte', 'Trabajo'];
+ const categoriesForm = document.getElementById('categories-form');
+ const categoriesList = document.getElementById('categories-list');
+
+
+categoriesForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const categoriesInput = document.getElementById('categories-input');
+    const categoryToAdd = categoriesInput.value;
+
+    categoriesArray.push(categoryToAdd);
+    
+
+    categoriesForm.reset();
+})
+
+
+ /* burger menu */
 const hamburgerMenu = document.getElementById('buttonMenu');
 const headerNav = document.getElementById('headerNav');
 
 buttonMenu.addEventListener('click', () => {
     headerNav.classList.toggle('hidden');
 })
+
